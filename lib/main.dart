7eb4a551058e7f43130d97da9b5819240725bf15
Hashishvilli <44935349+OlegBochkoV@users.noreply.github.com>
2022-08-23 22:38:02 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Star-Friends',
+      home: Scaffold(
+        body: SafeArea(
+          child: WebView(
+            initialUrl: 'https://star-friends.com',
+            onWebViewCreated: (WebViewController webViewController) {
+              FlutterNativeSplash.remove();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
